1671a6adaab6e3484ed3d95456b48c27f479f2b6
pavankumar1767 <karripavankumar1@gmail.com>
2025-03-20 16:48:22 +0530

    groups updated on 20th march

--- tests/User/test_TC04_dataExtraction.py
+++ tests/User/test_TC04_dataExtraction.py
@@ -9,6 +9,7 @@ from pages.job_page import JobPage
 from pages.login_page import LoginPage
 from utilities.DataStore import PropertyManager
 from utilities.TestDataManager import TestDataManager
+from utilities.random_utils import RandomUtils
 
 
 @allure.suite("data extraction with created user for single log")
@@ -22,6 +23,8 @@ class TestTC01:
         filter_page = FilterPage(page)
         job_page = JobPage(page)
 
+        filter_name = RandomUtils.get_last_name()
+
         login_page.navigate(config.BASE_URL)
         login_page.enter_username(PropertyManager.get_property("userName"))
         login_page.enter_password(config.setpassword)
@@ -32,9 +35,9 @@ class TestTC01:
         log = test_data["wells"]["log_1"]
         Object_list = test_data["wells"]["objectlist"]
 
-        home_page.select_module("/jobs")
-        filter_page.Button("Create Job")
-        # filter_page.Button("Skip")
+        home_page.select_module("/filter")
+        filter_page.assert_filterListPage()
+        filter_page.Button("Create")
         filter_page.input_field(well)
         filter_page.click_search("searchicon")
         filter_page.select_well(well)
@@ -47,13 +50,21 @@ class TestTC01:
         filter_page.select_logcurves(log)
         filter_page.click_log(log)
         filter_page.select_objects_and_select_all(Object_list)
+
         # extraction
-        filter_page.Button("Extract and Create Job")
-        filter_page.Button("Yes")
-        time.sleep(5)
+        filter_page.Button("Save and Create Job")
+        filter_page.enterFilterName(filter_name)
+        filter_page.clickButton("add Save")
+        filter_page.assert_popup("Filter saved successfully")
 
         # job summary
         job_id = job_page.get_jobnumber()
+        job_status = job_page.get_job_status(job_id)
+        assert job_status == "In Progress"
+        home_page.select_module("/filter")
+        filter_page.assert_filter_visible(filter_name)
+        home_page.select_module("/jobs")
+
         job_status = job_page.get_job_status(job_id)
         # job_page.assert_job_status(job_id)
         job_page.view_job(job_id)
